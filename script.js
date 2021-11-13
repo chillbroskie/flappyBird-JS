5b3c1c39a40cef39217da7c748380bfa6ef5d6f3
chillbroskie <64366196+chillbroskie@users.noreply.github.com>
2021-11-13 14:19:14 -0500

    finished game

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { updateBird, setupBird, getBirdRect } from './bird.js'
+import { updatePipes, setupPipes, getPastPipesCount, getPipReacts } from './pipe.js'
 
 
 document.addEventListener('keypress', handleStart, { once: true })
@@ -15,6 +16,7 @@ function updateLoop(time){
   }
   const delta = time - lastTime
   updateBird(delta)
+  updatePipes(delta)
   if (checkLose()) return handleLose()
   lastTime = time
   window.requestAnimationFrame(updateLoop)
@@ -22,13 +24,24 @@ function updateLoop(time){
 
 function checkLose() {
   const birdRect = getBirdRect()
+  const insidePipe = getPipReacts().some(rect => isCollision(birdRect, rect))
   const outsideWorld = birdRect.top < 0 || birdRect.bottom > window.innerHeight
-  return outsideWorld
+  return outsideWorld || insidePipe
+}
+
+function isCollision(rect1, rect2){
+  return (
+    rect1.left < rect2.right &&
+    rect1.top < rect2.bottom &&
+    rect1.right < rect2.left &&
+    rect1.bottom < rect2.top 
+  )
 }
 
 function handleStart() {
   title.classList.add('hide');
   setupBird();
+  setupPipes();
   lastTime = null
   window.requestAnimationFrame(updateLoop)
 }
@@ -37,7 +50,7 @@ function handleLose() {
   setTimeout(() => {
     title.classList.remove('hide');
     subtitle.classList.remove('hide');
-    subtitle.textContent = "0 Pipes"
+    subtitle.textContent = `${getPastPipesCount()} Pipes`
     document.addEventListener('keypress', handleStart, { once: true })
   }, 100)
 
